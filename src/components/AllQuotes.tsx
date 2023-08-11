595476e2940e41ef67f80c6eeec3dc8d099157b1
Mehmet <mehmetext68@gmail.com>
2023-08-11 10:19:11 +0300

    add QuoteItem component

--- src/components/AllQuotes.tsx
+++ src/components/AllQuotes.tsx
@@ -1,6 +1,20 @@
 import wait from "@/lib/utils/wait";
+import QuoteItem from "./QuoteItem";
 
 export default async function AllQuotes() {
-  await wait(1000);
-  return <div>AllQuotes</div>;
+  await wait(100);
+  return (
+    <div className="gap-x-4 space-y-4 sm:columns-2 md:columns-3">
+      <QuoteItem str="Assumenda deserunt nihil atque nesciunt quas, error tenetur nostrum in enim voluptates accusantium." />
+      <QuoteItem str="Lorem, ipsum dolor sit amet consectetur adipisicing elit." />
+      <QuoteItem str="Lorem ipsum." />
+      <QuoteItem str="Lorem, ipsum dolor sit amet consectetur adipisicing elit." />
+      <QuoteItem str="At blanditiis magnam perspiciatis illum sit reiciendis rerum earum." />
+      <QuoteItem str="Lorem, ipsum dolor sit amet consectetur adipisicing elit." />
+      <QuoteItem str="Assumenda deserunt nihil atque nesciunt quas, error tenetur nostrum in enim voluptates accusantium." />
+      <QuoteItem str="At blanditiis magnam perspiciatis illum sit reiciendis rerum earum." />
+      <QuoteItem str="At blanditiis magnam perspiciatis illum sit reiciendis rerum earum." />
+      <QuoteItem str="Assumenda deserunt nihil atque nesciunt quas, error tenetur nostrum in enim voluptates accusantium." />
+    </div>
+  );
 }
---
+++ src/components/QuoteItem.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+export default function QuoteItem({ str }: { str: string }) {
+  return (
+    <div className="bg-orange-100 break-inside-avoid rounded-md flex flex-col pt-8 gap-4 overflow-hidden relative">
+      <div className="absolute right-0 top-0 text-sm bg-red-500/30 text-red-700 font-medium px-2 py-1 rounded-bl-md">
+        -1
+      </div>
+      <p className="mx-4 font-medium">
+        <span className="font-thin pr-1">“</span>
+        {str}
+        <span className="font-thin pl-1">”</span>
+      </p>
+      <Link href="/" className="self-end flex items-center gap-2 mx-4">
+        <div className="flex flex-col text-right">
+          <p className="font-medium line-clamp-1 leading-none">
+            Mehmet Konukçu
+          </p>
+          <p className="text-xs text-black/40 leading-none">mehmetext</p>
+        </div>
+        <div className="shrink-0">
+          <img
+            src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/authors/1673611182i/3565._UX200_CR0,15,200,200_.jpg"
+            alt="Avatar"
+            className="w-8 h-8 rounded-full object-cover"
+          />
+        </div>
+      </Link>
+      <div className="grid grid-cols-2">
+        <button className="flex items-center justify-center py-2 text-sm bg-green-500/30 text-green-700 font-medium transition hover:bg-green-500/50">
+          Up
+        </button>
+        <button className="flex items-center justify-center py-2 text-sm bg-red-500/30 text-red-700 font-medium transition hover:bg-red-500/50">
+          Down
+        </button>
+      </div>
+    </div>
+  );
+}
