eada97afcd31160527f17b92214823555d9e0248
Mehmet <mehmetext68@gmail.com>
2023-08-11 14:46:27 +0300

    add formik and yup for auth

--- src/app/auth/login/components/Form.tsx
+++ src/app/auth/login/components/Form.tsx
@@ -1,32 +1,82 @@
 "use client";
 
+import * as Yup from "yup";
 import { useRouter } from "next/navigation";
+import { Formik } from "formik";
+import wait from "@/lib/utils/wait";
+
+const LoginSchema = Yup.object().shape({
+  username: Yup.string().required("Required").max(20, "Too long!"),
+  password: Yup.string().required("Required"),
+});
 
 export default function Form() {
   const router = useRouter();
 
   return (
-    <>
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        className="outline-none border rounded-md p-2 transition focus:border-orange-500"
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        className="outline-none border rounded-md p-2 transition focus:border-orange-500"
-      />
-      <button
-        onClick={() => {
-          router.replace("/");
-        }}
-        className="bg-orange-600 py-2 px-3 font-medium rounded transition hover:bg-orange-700 text-white"
-      >
-        Login
-      </button>
-    </>
+    <Formik
+      initialValues={{
+        username: "",
+        password: "",
+      }}
+      validationSchema={LoginSchema}
+      onSubmit={async (values) => {
+        await wait(500);
+      }}
+    >
+      {({
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+        touched,
+        values,
+        errors,
+      }) => {
+        return (
+          <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+            <label className="flex flex-col gap-1">
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                className="outline-none border rounded-md p-2 transition focus:border-orange-500"
+                value={values.username}
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+              {touched.username && errors.username && (
+                <span className="mx-2 text-red-500 text-xs">
+                  {errors.username}
+                </span>
+              )}
+            </label>
+            <label className="flex flex-col gap-1">
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="outline-none border rounded-md p-2 transition focus:border-orange-500"
+                value={values.password}
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+              {touched.password && errors.password && (
+                <span className="mx-2 text-red-500 text-xs">
+                  {errors.password}
+                </span>
+              )}
+            </label>
+            <button
+              disabled={isSubmitting}
+              type="submit"
+              className="bg-orange-600 py-2 px-3 font-medium rounded transition hover:bg-orange-700 text-white disabled:cursor-wait disabled:bg-orange-600/60"
+            >
+              Login
+            </button>
+          </form>
+        );
+      }}
+    </Formik>
   );
 }
--- src/app/auth/register/components/Form.tsx
+++ src/app/auth/register/components/Form.tsx
@@ -1,39 +1,120 @@
 "use client";
 
+import wait from "@/lib/utils/wait";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import { useRouter } from "next/navigation";
 
-export default function Form() {
+const RegisterSchema = Yup.object().shape({
+  username: Yup.string().required("Required").max(20, "Too long!"),
+  email: Yup.string().email("Invalid E-Mail").required("Required"),
+  password: Yup.string().required("Required"),
+  password2: Yup.string()
+    .oneOf([Yup.ref("password")], "Passwords must be matched")
+    .required("Required"),
+});
+
+export default function RegisterForm() {
   const router = useRouter();
 
   return (
-    <>
-      <input
-        type="text"
-        name="username"
-        placeholder="Username"
-        className="outline-none border rounded-md p-2 transition focus:border-orange-500"
-      />
-      <input
-        type="email"
-        name="email"
-        placeholder="E-Mail"
-        className="outline-none border rounded-md p-2 transition focus:border-orange-500"
-      />
-      <input
-        type="password"
-        name="password"
-        placeholder="Password"
-        className="outline-none border rounded-md p-2 transition focus:border-orange-500"
-      />
-      <input
-        type="password"
-        name="password2"
-        placeholder="Password Again"
-        className="outline-none border rounded-md p-2 transition focus:border-orange-500"
-      />
-      <button className="bg-green-600 py-2 px-3 font-medium rounded transition hover:bg-green-700 text-white">
-        Register
-      </button>
-    </>
+    <Formik
+      initialValues={{
+        username: "",
+        email: "",
+        password: "",
+        password2: "",
+      }}
+      validationSchema={RegisterSchema}
+      onSubmit={async (values) => {
+        await wait(500);
+      }}
+    >
+      {({
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+        touched,
+        values,
+        errors,
+      }) => {
+        return (
+          <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+            <label className="flex flex-col gap-1">
+              <input
+                type="text"
+                name="username"
+                placeholder="Username"
+                className="outline-none border rounded-md p-2 transition focus:border-orange-500"
+                value={values.username}
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+              {touched.username && errors.username && (
+                <span className="mx-2 text-red-500 text-xs">
+                  {errors.username}
+                </span>
+              )}
+            </label>
+            <label className="flex flex-col gap-1">
+              <input
+                type="email"
+                name="email"
+                placeholder="E-Mail"
+                className="outline-none border rounded-md p-2 transition focus:border-orange-500"
+                value={values.email}
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+              {touched.email && errors.email && (
+                <span className="mx-2 text-red-500 text-xs">
+                  {errors.email}
+                </span>
+              )}
+            </label>
+            <label className="flex flex-col gap-1">
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="outline-none border rounded-md p-2 transition focus:border-orange-500"
+                value={values.password}
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+              {touched.password && errors.password && (
+                <span className="mx-2 text-red-500 text-xs">
+                  {errors.password}
+                </span>
+              )}
+            </label>
+            <label className="flex flex-col gap-1">
+              <input
+                type="password"
+                name="password2"
+                placeholder="Password Again"
+                className="outline-none border rounded-md p-2 transition focus:border-orange-500"
+                value={values.password2}
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+              {touched.password2 && errors.password2 && (
+                <span className="mx-2 text-red-500 text-xs">
+                  {errors.password2}
+                </span>
+              )}
+            </label>
+            <button
+              disabled={isSubmitting}
+              type="submit"
+              className="bg-green-600 text-white py-2 px-3 font-medium rounded transition hover:bg-green-700 disabled:cursor-wait disabled:bg-green-600/60"
+            >
+              Register
+            </button>
+          </form>
+        );
+      }}
+    </Formik>
   );
 }
