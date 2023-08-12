f9d3f727fd97ffbbd66cf2891b798f3b33072040
Mehmet <mehmetext68@gmail.com>
2023-08-12 10:41:17 +0300

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,11 @@
+import Logo from "@/components/Logo";
 import React from "react";
 
 export default function AppLoading() {
   return (
-    <div className="bg-orange-500 h-screen w-screen text-white text-3xl flex items-center justify-center">
-      Loading...
+    <div className="bg-orange-500 h-screen w-screen flex items-center justify-center text-white flex-col gap-2">
+      <Logo />
+      <p className="text-lg">Loading...</p>
     </div>
   );
 }
