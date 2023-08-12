42a93d2738c748bb628611816d5661a913308d2b
Mehmet <mehmetext68@gmail.com>
2023-08-13 01:15:39 +0300

    Update LogoutButton.tsx

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -6,7 +6,7 @@ export default function LogoutButton() {
   return (
     <button
       onClick={() => {
-        signOut();
+        signOut({ callbackUrl: "/" });
       }}
       className="bg-red-600 py-2 px-3 font-medium rounded transition hover:bg-red-700"
     >
