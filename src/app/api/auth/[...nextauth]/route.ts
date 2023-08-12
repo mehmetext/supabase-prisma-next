b3b15d8773430675dfe7e187519a24861b5c105d
Mehmet <mehmetext68@gmail.com>
2023-08-12 17:59:27 +0300

    add nextauth and provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,40 @@
+import { prisma } from "@/lib/prisma";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    CredentialsProvider({
+      type: "credentials",
+      credentials: {
+        username: { label: "username", type: "text" },
+        password: { label: "password", type: "password" },
+      },
+      authorize: async (credentials, req) => {
+        if (!(credentials?.username && credentials.password)) {
+          throw new Error("Gerekli bilgiler girilmedi!");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            username: credentials.username,
+          },
+        });
+
+        if (!user) throw new Error("Username or password incorrect!");
+        if (await bcrypt.compare(credentials.password, user.hashedPassword))
+          throw new Error("Username or password incorrect!");
+
+        return user;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
