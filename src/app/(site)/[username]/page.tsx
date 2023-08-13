813903bddaa4072965d6f67315fbcce1cef55d89
Mehmet <mehmetext68@gmail.com>
2023-08-13 15:43:46 +0300

    add metadata to profile page

--- src/app/(site)/[username]/page.tsx
+++ src/app/(site)/[username]/page.tsx
@@ -1,3 +1,18 @@
+import { prisma } from "@/lib/prisma";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params: { username },
+}: {
+  params: { username: string };
+}): Promise<Metadata> {
+  const user = await prisma.user.findUnique({ where: { username } });
+
+  return {
+    title: `${user?.name} - SP Next`,
+  };
+}
+
 export default function Profile({
   params: { username },
 }: {
