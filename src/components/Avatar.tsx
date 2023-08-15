96d0ee2e6a92dd7fb586849d83644b143d2991b2
Mehmet <mehmetext68@gmail.com>
2023-08-15 12:50:19 +0300

    update avatar size

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,27 +3,30 @@ import cn from "@/lib/utils/cn";
 export default function Avatar({
   src,
   alt,
-  className,
+  size,
 }: {
   src?: string | null;
   alt: string;
-  className?: string;
+  size?: number;
 }) {
+  size = size ?? 32;
   const letter = alt.substring(0, 1);
+
   return (
     <>
       {src ? (
         <img
           src={src}
           alt={alt}
-          className={cn("w-8 h-8 rounded-full object-cover", className)}
+          className={cn("rounded-full object-cover")}
+          style={{ width: size, height: size }}
         />
       ) : (
         <span
           className={cn(
-            "w-8 h-8 flex items-center justify-center rounded-full bg-orange-700 text-white text-xs font-medium select-none",
-            className
+            "flex items-center justify-center rounded-full bg-orange-700 text-white font-medium select-none"
           )}
+          style={{ width: size, height: size, fontSize: size * 0.375 }}
         >
           {letter}
         </span>
--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -4,11 +4,7 @@ import Avatar from "./Avatar";
 export default async function ProfileInfo({ user }: { user: User }) {
   return (
     <div className="flex flex-col items-center gap-4">
-      <Avatar
-        alt={user.username}
-        src={user.image}
-        className="w-40 h-40 text-7xl"
-      />
+      <Avatar alt={user.username} src={user.image} size={160} />
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold">{user.name}</h1>
         <h4 className="text-2xl text-black/40">{user.username}</h4>
