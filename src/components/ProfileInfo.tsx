ef4c70e7cbedd81a363849663dca41fff421a63c
Mehmet <mehmetext68@gmail.com>
2023-08-15 11:57:31 +0300

    add name and username to profile page

--- src/components/ProfileInfo.tsx
+++ src/components/ProfileInfo.tsx
@@ -3,12 +3,16 @@ import Avatar from "./Avatar";
 
 export default function ProfileInfo({ user }: { user: User }) {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center gap-4">
       <Avatar
         alt={user.username}
         src={user.image}
         className="w-40 h-40 text-7xl"
       />
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold">{user.name}</h1>
+        <h4 className="text-2xl text-black/40">{user.username}</h4>
+      </div>
     </div>
   );
 }
