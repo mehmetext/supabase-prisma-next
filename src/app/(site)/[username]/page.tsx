b1b8efe80b0e70d4969711886ca3bb6685a1b577
Mehmet <mehmetext68@gmail.com>
2023-08-15 12:16:03 +0300

    Update page.tsx

--- src/app/(site)/[username]/page.tsx
+++ src/app/(site)/[username]/page.tsx
@@ -35,7 +35,7 @@ export default async function Profile({
       user: true,
       quoteAndVote: {
         where: {
-          userId: user?.id,
+          userId: me?.id,
         },
       },
     },
